de4c4557554e404feef9fd2cbd64c24e7b5dafaf
Great-Stone <hahohh@gmail.com>
2024-10-18 11:05:56 +0900

    instruqt instance type limit

--- setup-openstack/variables.tf
+++ setup-openstack/variables.tf
@@ -1,5 +1,5 @@
 variable "instance_type" {
   # default     = "m7i-flex.2xlarge"
-  default     = "t3.2xlarge"
+  default     = "t3.xlarge" # Instruqt allowed largest instance.
   description = "Instance type to use for the instance."
 }
